828f233f6da73344f3d31b9f2ce4fee44d931394
Michal <mkurdziel1987@gmail.com>
2021-08-06 11:43:05 +0200

    Add team members

---
+++ thankyou/index.php
@@ -0,0 +1,23 @@
+<?php include '../header.php'; ?>
+
+<div class="hero_section_front">
+	<div class="container_pp relativ_pos">
+	<div class="svg_line1_hero svg_line_position" data-aos="fade-right" data-aos-duration="1000">
+			<svg xmlns="http://www.w3.org/2000/svg" width="1379.508" height="18.293" viewBox="0 0 1379.508 18.293"> <g id="Group_3660" data-name="Group 3660" transform="translate(999.004 -259.851)"> <line id="Line_2" data-name="Line 2" x2="1379.5" y2="5.563" transform="translate(-999 263.5)" fill="none" stroke="#fff" stroke-width="2"/> <g id="Ellipse_651" data-name="Ellipse 651" transform="translate(170 278.144) rotate(-90)" fill="#273142" stroke="#fe0" stroke-width="2"> <circle cx="9.146" cy="9.146" r="9.146" stroke="none"/> <circle cx="9.146" cy="9.146" r="8.146" fill="none"/> </g> <g id="Ellipse_652" data-name="Ellipse 652" transform="translate(198.271 274.817) rotate(-90)" fill="#273142" stroke="#fff" stroke-width="2"> <circle cx="5.82" cy="5.82" r="5.82" stroke="none"/> <circle cx="5.82" cy="5.82" r="4.82" fill="none"/> </g> <g id="Ellipse_653" data-name="Ellipse 653" transform="translate(263.128 274.817) rotate(-90)" fill="#273142" stroke="#fff" stroke-width="2"> <circle cx="5.82" cy="5.82" r="5.82" stroke="none"/> <circle cx="5.82" cy="5.82" r="4.82" fill="none"/> </g> <g id="Ellipse_654" data-name="Ellipse 654" transform="translate(241.509 274.817) rotate(-90)" fill="#273142" stroke="#fff" stroke-width="2"> <circle cx="5.82" cy="5.82" r="5.82" stroke="none"/> <circle cx="5.82" cy="5.82" r="4.82" fill="none"/> </g> <g id="Group_3656" data-name="Group 3656" transform="translate(284.747 263.177)"> <g id="Ellipse_655" data-name="Ellipse 655" transform="translate(0 11.641) rotate(-90)" fill="#273142" stroke="#fff" stroke-width="2"> <circle cx="5.82" cy="5.82" r="5.82" stroke="none"/> <circle cx="5.82" cy="5.82" r="4.82" fill="none"/> </g> </g> <g id="Ellipse_656" data-name="Ellipse 656" transform="translate(219.89 274.817) rotate(-90)" fill="#273142" stroke="#fff" stroke-width="2"> <circle cx="5.82" cy="5.82" r="5.82" stroke="none"/> <circle cx="5.82" cy="5.82" r="4.82" fill="none"/> </g> </g> </svg>
+		</div>
+		<div class="two_columns_hero">
+			<div class="left_col_hero">
+				<div class="box_left_col_hero">
+					<h1 class="h1_hero"><span class="asdfkdsakf"><span class="sec_txt_hero">Congratulations!</span><br>You will be the first to know when we launch our public sale.</span></h1>
+				</div>
+			</div>
+		<div class="paralax_image_mouse">
+				<img src="/image/starterra_statek.png" class="space_paralax">
+				<img src="/image/armada.png" class="space_paralax2">
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../footer.php'; ?>
