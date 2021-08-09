f566fc1aff6427b40ae856876e051f30d4d8d60d
skalbam <mskalba@eworklabs.io>
2021-08-09 23:46:47 +0200

    Whitepaper

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
 		<li class="menu_item"><a href="/#tokenomics">Tokenomics</a></li>
 		<li class="menu_item"><a href="/#road-map">Road Map</a></li>
 		<li class="menu_item"><a href="/#team">Team</a></li>
-		<li class="menu_item"><a href="#" class="btn btn-secondary coming_soon_btn">Coming soon</a></li>
+		<li class="menu_item"><a href="https://docs.google.com/forms/d/1-ZTTi4KW_7n8rcJ6J97MAMh_SImoyIdOc7B_HYtg_XI/" target="_blank" class="btn btn-secondary apply_ido_btn">Apply for IDO</a></li>
 		<?php /* <li class="menu_item menu_item_button_last"><a href="#">Apply for IDO</a></li> */ ?>
 	</ul>
 </nav>
